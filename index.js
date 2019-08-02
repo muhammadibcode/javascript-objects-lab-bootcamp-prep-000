f0fc39b77c54f7b1d09256d882d588b65f080685
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-02 20:07:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var objectclone = object
-  delete objectclone[key];
+  
   return objectclone;
 }
